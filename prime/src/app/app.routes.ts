93c1cc8f0c738502bb45f18197b257c93a6c559d
mhn098 <mhn098@ad.unc.edu>
2024-06-26 22:03:37 -0400

    fix routes

--- prime/src/app/app.routes.ts
+++ prime/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MainPageComponent } from './main-page/main-page.component';
+
+export const routes: Routes = [
+  MainPageComponent.Route,
+{
+  path: 'main-page',
+  title: 'Main Page',
+  loadComponent: () =>
+    import('./main-page/main-page.component').then(
+      (m) => m.MainPageComponent
+    )
+},];
+// { path: '', redirectTo: 'main-page', pathMatch: 'full' }, // default route
+// { path: '**', redirectTo: 'main-page' }]; // wildcard route
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class AppRoutingModule { }
